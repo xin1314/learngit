e18f821ec14119df041e6fcce6952f6c7d81a47f
xin1314 <946986670@qq.com>
2019-11-01 13:22:24 +0800

    创建一个名称为mrsoft.db的SQLite数据库文件，并创建user（用户表）

---
+++ SqliteLearn/Sqlite3.py
@@ -0,0 +1,7 @@
+import sqlite3
+conn = sqlite3.connect('mrsoft.db')
+cursor = conn.cursor()
+cursor.execute('create table user (id int(10) primary key, name varchar(20))')
+cursor.close()
+conn.close()
+
