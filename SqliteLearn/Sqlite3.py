3998c04f5e17de7a4b303c39a836aa4d502aa4d3
xin1314 <946986670@qq.com>
2019-11-01 13:25:24 +0800

    新增用户数据信息

--- SqliteLearn/Sqlite3.py
+++ SqliteLearn/Sqlite3.py
@@ -1,7 +1,10 @@
 import sqlite3
 conn = sqlite3.connect('mrsoft.db')
 cursor = conn.cursor()
-cursor.execute('create table user (id int(10) primary key, name varchar(20))')
-cursor.close()
-conn.close()
+# cursor.execute('create table user (id int(10) primary key, name varchar(20))')
+# cursor.close()
+# conn.close()
 
+cursor.execute('insert into user (id, name) values ("1", "MRSOFT")')
+cursor.execute('insert into user (id, name) values ("2", "Andy")')
+cursor.execute('insert into user (id, name) values ("3", "明日科技小助手")')
